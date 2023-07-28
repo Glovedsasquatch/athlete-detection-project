a0ffa9ac021e50cb8d0b015c0ded7283f4b0ec31
Glovedsasquatch <prabhashankar@tum.de>
2023-07-28 17:54:34 +0200

    Initial setup for OpenCV and CMakeLists.txt

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main() {
+    std::cout << "Hello, World!, the OpenCV version is: " << cv::getVersionString() << std::endl;
+    return 0;
+}
